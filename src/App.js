599ebab54671d6cae78ea80f7cca9ebae33fa33d
tothemoon6789 <pnlinh6789@gmail.com>
2022-12-21 21:35:11 +0700

    delete react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import "./style.scss"
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Answers from './component/answer';
-import Layout from './component/layout';
-import Nopage from './component/nopage';
-import Review from './component/review';
 import Developer from './developer';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-          <Route path='/' element={<Layout/>}>
-            <Route index element={<Developer/>}/>
-            <Route path='review' element={<Review/>}/>
-            <Route path='*' element={<Nopage/>}/>
-          </Route>
-      </Routes>
-    </BrowserRouter>
+   <Developer/>
   );
 };
 
